fe057b5aeb81d0105e1b226dee73d485c47ea9c7
Luke Hill <lh325@njit.edu>
2024-12-15 20:18:01 -0500

    modified file names

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 cmake ..
 make
-cat ../testin | ./PA3 -K dot
-cat ../testin | ./PA3 -K dot -Tpng -o PA3.png
+cat ../testin.txt | ./PA3 -K dot
+cat ../testin.txt | ./PA3 -K dot -Tpng -o PA3.png
